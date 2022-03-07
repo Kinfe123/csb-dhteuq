cc917950506fa1d801deeeddcecf4e8069838ce7
Code Sandbox <hello@codesandbox.io>
2022-03-07 23:14:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.25ab387ba9341e3e617cb03a5733e39f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1271c52314dc7152b5aabc1b572266b3",
+    "url": "https://kinfe123.github.io/csb-dhteuq/index.html"
+  },
+  {
+    "revision": "4ac4d6d444c29d1d60b5",
+    "url": "https://kinfe123.github.io/csb-dhteuq/static/css/main.cca13aff.chunk.css"
+  },
+  {
+    "revision": "a8e04fb5e4cf30616317",
+    "url": "https://kinfe123.github.io/csb-dhteuq/static/js/2.d4e4e489.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "https://kinfe123.github.io/csb-dhteuq/static/js/2.d4e4e489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac4d6d444c29d1d60b5",
+    "url": "https://kinfe123.github.io/csb-dhteuq/static/js/main.75e93d75.chunk.js"
+  },
+  {
+    "revision": "b8992e54e28541875569",
+    "url": "https://kinfe123.github.io/csb-dhteuq/static/js/runtime-main.0ca561f9.js"
+  }
+]);
